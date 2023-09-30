026502e46df4107bb60ff643fcc491ca42e585c5
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-09-30 17:16:18 +0100

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+  const form = document.querySelector("form");
+  form.onsubmit = (e) => {
+    e.preventDefault();
+    const id = e.target.children[1].value;
+    
+    fetch("http://localhost:3000/movies/" + id)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector("section#movieDetails h4");
+            const summary = document.querySelector("section#movieDetails p");
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+        .catch(error => console.log(error.message));
+  }
 }
 
 document.addEventListener('DOMContentLoaded', init);
